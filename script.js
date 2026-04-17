b03039afb527c4ccaa80763f2db0d2ac94c89c0c
CommeBae <junbe0504@gmail.com>
2026-04-17 22:39:53 +0900

    묭

--- script.js
+++ script.js
@@ -117,7 +117,7 @@
     };
     setMeta('property', 'og:title', m.title);
     setMeta('property', 'og:description', m.description);
-    setMeta('property', 'og:image', 'images/og/ㄴ.jpg');
+    setMeta('property', 'og:image', 'images/og/2.jpg');
     setMeta('name', 'description', m.description);
   }
 
